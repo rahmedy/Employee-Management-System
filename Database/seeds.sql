edcb5d4eb94a188cebfab5a08abaf3476e468bc3
Rayan Ahmedy <rayanahmedy@Rayans-MacBook-Pro.local>
2020-09-08 02:01:07 -0400

    setting up database

--- Database/seeds.sql
+++ Database/seeds.sql
@@ -1,14 +1,39 @@
 -- Departments
 USE employee_data;
 
-INSERT INTO departments (dptid, department_name)
+INSERT INTO departments (id, department_name)
 VALUES (1, "Sales");
 
-INSERT INTO departments (dptid, department_name)
-VALUES (2, "Engineering");
+INSERT INTO departments (id, department_name)
+VALUES (2, "Human Resources");
 
-INSERT INTO departments (dptid, department_name)
-VALUES (3, "Finance");
+INSERT INTO departments (id, department_name)
+VALUES (3, "IT");
 
-INSERT INTO departments (dptid, department_name)
-VALUES (4, "Legal");
+INSERT INTO departments (id, department_name)
+VALUES (4, "Finance");
+
+
+-- Roles
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Architect ", 110000, 1);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Specialist", 90000, 1);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Human resources specialist", 120000, 2);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Cloud  Engineer", 950000, 3);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Software Engineer", 100000, 3);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Accountant", 115000, 4);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Financial Analyst", 125000, 4);
+
+
